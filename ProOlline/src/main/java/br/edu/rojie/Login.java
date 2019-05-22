0dcfe3a2bd1b0a4f0bac41dc0fdee76ff08a823f
Rojie1001 <rojieemanuell@gmail.com>
2019-05-21 23:31:21 -0300

    Criação de Outras Classes e ajustes
    Criado Classe Main
    Classe Error
    Classe String
    E feito ajustes dos botões da tela login

--- ProOlline/src/main/java/br/edu/rojie/Login.java
+++ ProOlline/src/main/java/br/edu/rojie/Login.java
@@ -2,6 +2,9 @@ package br.edu.rojie;
 
 import javax.swing.JOptionPane;
 
+
+import br.edu.rojie.Error;
+import br.edu.rojie.Strings;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,63 +15,82 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+
 public class Login extends Application {
 
 	private AnchorPane pane1;
 	private TextField txLogin;
 	private PasswordField txSenha;
-	private Button btEntrar, btSair;
+	private Button button;
 	private static Stage stage;
-
 	
-	  @Override 
-	  public void start(Stage stage) throws Exception { 
-	  AnchorPane pane1 = new AnchorPane(); pane1.setPrefSize(300, 200); TextField
-	  txLogin = new TextField(); txLogin.setPromptText("Username"); PasswordField
-	  txSenha = new PasswordField(); txSenha.setPromptText("Digite sua senha");
-	  Button btEntrar = new Button("Entrar"); Button btSair = new Button("Sair");
-	  pane1.getChildren().addAll(txLogin, txSenha, btEntrar, btSair);
-	 
-	  Scene scene = new Scene(pane1); stage.setScene(scene); stage.show();
-	  txLogin.setLayoutX((pane1.getWidth() - txLogin.getWidth()) / 2);
-	  txLogin.setLayoutY(50); txSenha.setLayoutX((pane1.getWidth() -
-	  txSenha.getWidth()) / 2); txSenha.setLayoutY(80);
-	  btEntrar.setLayoutX((pane1.getWidth() - btEntrar.getWidth()) / 2);
-	  btEntrar.setLayoutY(110); btSair.setLayoutX((pane1.getWidth() -
-	  btSair.getWidth()) / 2); btSair.setLayoutY(140); pane1.
-	  setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, pink 0%, silver 500%);"
-	  ); }
-	 
-
-	private void initListeners() {
-		btSair.setOnAction(new EventHandler<ActionEvent>() {
-			// @Override
-			public void handle(ActionEvent event) {
-				fecharAplicacao();
-			}
-		});
-	}
-
-	private void fecharAplicacao() {
-		System.exit(0);
-		btEntrar.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				Entrar();
-			}
-		});
-	}
-
-	private void Entrar() {
-		if (txLogin.getText().equals("admin") && txSenha.getText().equals("admin")) {
-		} 
-	}
-
-	public static Stage getStage() {
-		return stage;
-	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		launch(args);
 	}
+
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		AnchorPane pane1 = new AnchorPane();
+		pane1.setPrefSize(300, 200);
+		TextField txLogin = new TextField();
+		txLogin.setPromptText("Username");
+		PasswordField txSenha = new PasswordField();
+		txSenha.setPromptText("Digite sua senha");
+		Button btEntrar = new Button("Entrar");
+		Button btSair = new Button("Sair");
+		pane1.getChildren().addAll(txLogin, txSenha, btEntrar, btSair);
+
+		Scene scene = new Scene(pane1);
+		stage.setScene(scene);
+		stage.show();
+		txLogin.setLayoutX((pane1.getWidth() - txLogin.getWidth()) / 2);
+		txLogin.setLayoutY(50);
+		txSenha.setLayoutX((pane1.getWidth() - txSenha.getWidth()) / 2);
+		txSenha.setLayoutY(80);
+		btEntrar.setLayoutX((pane1.getWidth() - btEntrar.getWidth()) / 2);
+		btEntrar.setLayoutY(110);
+		btSair.setLayoutX((pane1.getWidth() - btSair.getWidth()) / 2);
+		btSair.setLayoutY(140);
+		pane1.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, pink 0%, silver 500%);");
+	}
+
+	private void login(Stage stage, int password) {
+
+		if (txLogin.getText().isEmpty()) {
+			try {
+				new Error(Strings.loginError).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}	
+	
+		if (txLogin.getText().equals("admin")) {
+			try {
+				new Error(Strings.loginError).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+	}
+		if (!txSenha.getText().equals("admin")) {
+			try {
+				new Error(Strings.passwordError).start(new Stage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return;
+		}
+	
+	try {
+		new Main(txLogin.getText()).start(new Stage());
+		stage.close();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+
+	}
+
 
 }
---
+++ ProOlline/src/main/java/br/edu/rojie/Main.java
@@ -0,0 +1,49 @@
+package br.edu.rojie;
+
+import javafx.scene.control.Label;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+
+public class Main extends Application {
+
+	public Main(String text) {
+		// TODO Auto-generated constructor stub
+	}
+	@Override 
+	  public void start(Stage stage) throws Exception {
+		AnchorPane pane2 = new AnchorPane();
+		pane2.setPrefSize(500, 400); 
+		JComboBox combo = new JComboBox();
+		Button btnMeusCursos = new Button("Meus Cursos");
+		Button btnAllCursos = new Button("Todos os Cursos");
+		Label label = new Label("Bons Estudos Jovem Padauã");
+		
+		pane2.getChildren().addAll( btnMeusCursos, btnAllCursos, label);
+		pane2.getChildren().add(combo);
+		 
+		
+		Scene scene = new Scene(pane2);
+		stage.setScene(scene);
+		stage.show();
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
